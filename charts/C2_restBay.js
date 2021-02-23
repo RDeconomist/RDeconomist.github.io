fead82c32d549921be96f3e3949e7cd096d86b44
Richard Davies <richard.davies.mobile@gmail.com>
2021-02-23 14:44:05 +0000

    Added more surf

---
+++ charts/C2_restBay.js
@@ -0,0 +1,47 @@
+var chartC2 =
+{
+  "data": {
+    "url": "https://raw.githubusercontent.com/RDeconomist/rapidcharts/master/waves/waveHeights_today.csv"
+  },
+
+   "title": {
+    "text": "Rest Bay",
+    "subtitle":"Today's 1-week swell forecast, from MSW",
+    "subtitleFontStyle":"italic",
+    "subtitleFontSize":10,
+    "anchor": "start",
+    "color": "black"
+  },
+  
+  "height":300,
+  "width":350,
+
+  "mark": {
+    "type":"area",
+    "color":"#0BB5FF"
+      },
+    
+  "encoding": {
+      
+      "x": {
+        "field": "days",
+        "type": "quantitative",
+        "title": "Forecast (days ahead)"        
+      },
+      
+      "y": {
+        "field": "restbay_high",
+        "type": "quantitative",
+        "title": "Predicted wave height",
+        "grid":null
+      },
+
+      "y2": {
+        "field": "restbay_low",
+        "type": "quantitative",
+        "title": "Predicted wave height",
+        "grid":null
+      }
+    }
+  };
+vegaEmbed('#chartC2', chartC2, {"actions": false});
