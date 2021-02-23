f9b258bde9dccdb17e459cf39824973c91a87970
Richard Davies <richard.davies.mobile@gmail.com>
2021-02-23 15:18:03 +0000

    Added new charts

---
+++ charts/C8_freshwatereast.js
@@ -0,0 +1,47 @@
+var chartC8 =
+{
+  "data": {
+    "url": "https://raw.githubusercontent.com/RDeconomist/rapidcharts/master/waves/waveHeights_today.csv"
+  },
+
+   "title": {
+    "text": "Llangennith",
+    "subtitle":"Today's 1-week swell forecast, from MSW",
+    "subtitleFontStyle":"italic",
+    "subtitleFontSize":10,
+    "anchor": "start",
+    "color": "black"
+  },
+  
+  "height":300,
+  "width":350,
+
+  "mark": {
+    "type":"area",
+    "color":"#0BB5FF"
+      },
+    
+  "encoding": {
+      
+      "x": {
+        "field": "days",
+        "type": "quantitative",
+        "title": "Forecast (days ahead)"        
+      },
+      
+      "y": {
+        "field": "llangennith_high",
+        "type": "quantitative",
+        "title": "Predicted wave height",
+        "grid":null
+      },
+
+      "y2": {
+        "field": "llangennith_low",
+        "type": "quantitative",
+        "title": "Predicted wave height",
+        "grid":null
+      }
+    }
+  };
+vegaEmbed('#chartC8', chartC8, {"actions": false});
