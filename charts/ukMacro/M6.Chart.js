94741a78e7366c6306e40c891ec4f98d5a1c6269
MeyrickCharlieEcon <75495274+MeyrickCharlieEcon@users.noreply.github.com>
2021-03-19 16:59:05 +0000

    M6 added
    bar chart

---
+++ charts/ukMacro/M6.Chart.js
@@ -0,0 +1,29 @@
+var ChartM6 =
+
+{
+  "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+  "description": "(M6) Central bank independence",
+
+"title": {
+    "text": "Central bank independence",
+    "subtitle":"Source: BoE",
+    "subtitleFontStyle":"italic",
+    "subtitleFontSize":10,
+    "anchor": "start",
+    "color": "black"
+  },
+
+  "data": {
+    "url": "https://raw.githubusercontent.com/RDeconomist/RDeconomist.github.io/main/charts/ukMacro/M6.DataHub.MonetaryCBIndependence.csv"
+  },
+  "height": 300,
+  "width": 345,
+  "mark": {"type": "bar", "color": "darkblue"},
+  "encoding": {
+    "y": {"field": "Country", "type": "nominal", "title": "", "sort": "-x"},
+    "x": {"aggregate": "sum", "field": "Value", "title": "", "axis": {"grid": false}}
+  }
+};
+
+
+vegaEmbed('#ChartM6', ChartM6, {"actions": false});
