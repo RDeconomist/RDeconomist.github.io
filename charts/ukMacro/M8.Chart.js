59a504a9fd2087b68cf88426b0f3219dace062d5
MeyrickCharlieEcon <75495274+MeyrickCharlieEcon@users.noreply.github.com>
2021-03-23 16:58:14 +0000

    Added M8-10, edited html
    More macro metrics added

---
+++ charts/ukMacro/M8.Chart.js
@@ -0,0 +1,89 @@
+var ChartM8 =
+
+{
+  "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+
+  "description": "(M8) UK Gov bond rate",
+
+   "title": {
+    "text": "Government bonds",
+    "subtitle":"Average discount rate, 3-month Treasury Bill. Source: BoE",
+    "subtitleFontStyle":"italic",
+    "subtitleFontSize":10,
+    "anchor": "start",
+    "color": "black"
+  },
+  
+  "data": {
+    "url": "https://raw.githubusercontent.com/RDeconomist/RDeconomist.github.io/main/charts/ukMacro/M7.DataHub.Monetary.UKTreasuryBonds.csv"},
+  "height": 300,
+  "width": 345,
+  
+  "encoding": {"x": {"field": "Date", "type": "temporal", "axis": {"title":null, "grid": false}}},
+
+  "layer": [
+
+    {"encoding": {
+
+        "y": {"field": "Value", 
+
+              "type": "quantitative", 
+              "title":"",
+              "scale": {"domain": [0, 18]},
+               "axis": {"grid": false}
+              }
+            },
+
+        
+        "layer": [{"mark": {"type": "line", "color": "darkblue"}},
+
+                  {"transform": 
+
+                      [{"filter": {"selection": "hover"}}], 
+
+                        "mark": "point"}
+
+      ]
+
+    },
+
+    {"mark": "rule", 
+
+      "encoding": {
+        "opacity": {
+          "condition": {"value": 0.1, "selection": "hover"},
+          "value": 0
+        },
+
+        "tooltip": [
+
+          {"field": "Date", "type": "temporal", "format":"%B, %Y"},
+          {"field": "Value", "title": "Discount rate", "type": "quantitative", "format": ""}
+
+        ]
+
+      },
+
+
+      
+      "selection": {
+        "hover": {
+          "type": "single",
+          "fields": ["Date"],
+          "nearest": true,
+          "on": "mouseover",
+          "empty": "none",
+          "clear": "mouseout"
+
+        }
+
+      }
+
+    }
+
+  ]
+
+};
+
+
+vegaEmbed('#ChartM8', ChartM8, {"actions": false});
